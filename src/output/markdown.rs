544630bf12c9319542ab2a24bb614ba95b929e6b
tcrichardson <t.c.richardson@gmail.com>
2026-05-07 17:23:23 -0400

    feat: add cognitive load columns to markdown output

--- src/output/markdown.rs
+++ src/output/markdown.rs
@@ -14,22 +14,29 @@ impl OutputFormatter for MarkdownFormatter {
                 continue;
             }
             out.push_str(&format!(
-                "### {} (total complexity: {}, total lines: {}, functions: {})\n\n",
+                "### {} (total complexity: {}, total lines: {}, functions: {}, avg cognitive load: {:.2}, max nesting: {}, avg Halstead volume: {:.2})\n\n",
                 file.path.display(),
                 file.total_complexity,
                 file.total_lines,
-                file.function_count
+                file.function_count,
+                file.avg_cognitive_load,
+                file.max_nesting_depth,
+                file.avg_halstead_volume
             ));
-            out.push_str("| Function | Lines | Line Range | Complexity |\n");
-            out.push_str("|----------|-------|------------|------------|\n");
+            out.push_str("| Function | Lines | Line Range | Complexity | Nesting | Halstead Vol | Difficulty | Cognitive Load |\n");
+            out.push_str("|----------|-------|------------|------------|---------|--------------|------------|----------------|\n");
             for func in &file.functions {
                 out.push_str(&format!(
-                    "| {} | {} | {}-{} | {} |\n",
+                    "| {} | {} | {}-{} | {} | {} | {:.2} | {:.2} | {:.2} |\n",
                     func.name,
                     func.lines,
                     func.line_start,
                     func.line_end,
-                    func.complexity
+                    func.complexity,
+                    func.nesting_depth,
+                    func.halstead_volume,
+                    func.halstead_difficulty,
+                    func.cognitive_load
                 ));
             }
             out.push('\n');
